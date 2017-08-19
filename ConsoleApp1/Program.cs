ecf59f1e4a285a28407e179915328dd896375b3d
Labasse <Labasse>
2017-08-19 23:10:47 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("welcome to my first console App");
+        Console.ReadKey();
+        }
+    }
+
